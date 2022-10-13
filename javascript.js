0905cc253a184e0483093ce854fc8b4c8dc075f8
Ram <randallamalcolm@gmail.com>
2022-10-12 21:14:21 -0400

    finished

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 let computerChoice = "";
-let playerChoice = "Scissors";
+let playerChoice = "";
 
 function getComputerChoice() {
-    alert("Running getComputerChoice");
     let computerNum = Math.floor(Math.random() * 3);
     if (computerNum === 0) {
         computerChoice = "Rocks";
@@ -10,43 +9,44 @@ function getComputerChoice() {
         computerChoice = "Paper";   
     } else {
         computerChoice = "Scissors";
-
     };
-    alert(computerChoice); 
+    alert("RobotChoice: " + computerChoice);
 }
 
 function battleRoyale() {
-    alert("Runninng battleRoyale! ");
-    if (computerChoice = "Rocks") {
-        if (playerChoice = "Rocks") {
+    alert("Your Choice: " + playerChoice);
+    if (computerChoice == "Rocks") {
+        if (playerChoice == "Rocks") {
+            alert("playerChoice is " + playerChoice + " ...This was selected!");
             alert("TIE! Robot: Rocks, You: Rocks");
-        } else if (playerChoice = "Paper") {
+        } else if (playerChoice == "Paper") {
             alert("YOU WIN! Robot: Rocks, You: Paper")
-        } else if (playerChoice = "Scissors") {
+        } else if (playerChoice == "Scissors") {
             alert("ROBOT WINS! Robot: Rocks, You: Scissors")
         }
-    } else if (computerChoice = "Paper") {
-        if (playerChoice = "Rocks") {
+    } else if (computerChoice == "Paper") {
+        if (playerChoice == "Rocks") {
             alert("ROBOT WINS! Robot: Paper, You: Rocks");
-        } else if (playerChoice = "Paper") {
+        } else if (playerChoice == "Paper") {
             alert("TIE! Robot: Paper, You: Paper")
-        } else if (playerChoice = "Scissors") {
+        } else if (playerChoice == "Scissors") {
             alert("YOU WIN! Robot: Paper, You: Scissors")
         }
-    } else if (computerChoice = "Scissors") {
-        if (playerChoice === "Rocks") {
+    } else if (computerChoice == "Scissors") {
+        if (playerChoice == "Rocks") {
             alert("YOU WIN! Robot: Scissors, You: Rocks");
-        } else if (playerChoice = "Paper") {
+        } else if (playerChoice == "Paper") {
             alert("ROBOT WINS! Robot: Scissors, You: Paper")
-        } else if (playerChoice = "Scissors") {
+        } else if (playerChoice == "Scissors") {
             alert("TIE! Robot: Scissors, You: Scissors")
         }
     }
 }
 
-
-
 function commenceBattle() {
+    let activeChoice = document.getElementById("mySelector").value;
+    alert("OK, you've chosen " + activeChoice + "... hold your breath!");
+    playerChoice = activeChoice;
     getComputerChoice();
     battleRoyale(playerChoice);       
 }
